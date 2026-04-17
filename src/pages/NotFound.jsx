4d3a1f27c85c4594a133aefad8405330d9e68875
Ameravin <bodia050609@gmail.com>
2026-04-18 00:15:07 +0300

    Update NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="page">
+      <div className="container">
+        <h1 className="section-title">404 - Сторінку не знайдено</h1>
+        <p className="section-text">Такої сторінки не існує.</p>
+      </div>
+    </div>
+  )
+}
